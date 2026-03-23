a7ee93552d8758d14bf9b390b428341259f998b6
Hyunwoo Yoon <hyunwooyoon@Hyunwoo-Yoon-MacBook-Pro.local>
2026-03-23 15:29:35 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import V1 from './pages/v1'
 import V2 from './pages/v2'
+import V3 from './pages/v3'
 
 function App() {
   const path = window.location.pathname
 
+  if (path.startsWith('/v3')) return <V3 />
   if (path.startsWith('/v2')) return <V2 />
   return <V1 />
 }
